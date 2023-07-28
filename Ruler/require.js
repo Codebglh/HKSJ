4a02f42024397f350dd450d3462f547abcb97c19
bgcode <bglhcode@qq.com>
2023-07-29 00:18:31 +0800

    bgcode 上传规则首页规则

--- Ruler/require.js
+++ Ruler/require.js
@@ -1,8 +1,5 @@
 let rulerurl = "https://cdn.jsdelivr.net/gh/Codebglh/HKSJ@0.0.1/Ruler/main.js";
 let relyruler = "";
-if(fileExist('hiker://files/libs/' + md5(rulerurl) + '.js')){
-    relyruler = rulerurl;
-}
 if(relyruler==""){
     relyruler = rulerurl;
 
