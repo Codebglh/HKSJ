4997ffc9e9b6e9f6acbeee0df3497401b9a39e28
bgcode <bglhcode@qq.com>
2023-08-09 01:41:43 +0800

    bgcode 修改图标小程序错误

--- Ruler/FQ.js
+++ Ruler/FQ.js
@@ -1,20 +1,26 @@
-function F(){
-    var url_head="https://ghproxy.com/https://github.com/Kr328/ClashForAndroid/releases/download/v2.5.12/cfa-2.5.12-";
-    var url_back=[
-        "foss-universal-release.apk",
-        "premium-universal-release.apk",
-        ]
-    var name=["foss-clash","premium-clash"]
-    var  display=[];
-
-    for(let i in name){
-        display.push({
-            title:name,
-            col_type: "icon_4",
-            url:url_head+url_back[i],
-            pic_url:"hiker://files/bgHouse/src/system/clashLogo.png"
-        })
+function F() {
+  var display = [];
+  var data = JSON.parse(
+    fetch("https://cdn.jsdelivr.net/gh/Codebglh/HKSJ@0.0.1/Ruler/FQ.json")
+  );
+  var app_list = data.list;
+  for (let i in app_list) {
+    display.push({
+      title: app_list[i],
+      col_type: "text_1",
+      url: "hiker://empty",
+    });
+    let app_detail = JSON.parse(data.app_list[i]);
+    let names = app_detail.name;
+    for (let j in names) {
+      display.push({
+        title: names[j],
+        col_type: "icon_4",
+        url: app_detail.url[j],
+        pic_url: app_detail.pic_url[j],
+      });
     }
-    setResult(display);
-}
+  }
 
+  setResult(display);
+}
