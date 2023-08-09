f345dc49f6d0181d7d1a2a693750129835870e68
bgcode <bglhcode@qq.com>
2023-08-09 08:12:23 +0800

    bgcode 修改FQ小程序

--- Ruler/FQ.js
+++ Ruler/FQ.js
@@ -1,3 +1,16 @@
+function getColor(){
+  var colors = [
+    "#83559e",
+    "#ff7512",
+    "#cb54ff",
+    "#ff5358",
+    "#ff9044",
+    "#7678ff",
+    "#34b1ff",
+  ];//颜色可自行添加
+  return colors[Math.floor(Math.random() * colors.length)]//返回随机颜色
+}
+
 function F() {
   var display = [];
   var data = JSON.parse(
@@ -6,11 +19,11 @@ function F() {
   var app_list = data.list;
   for (let i in app_list) {
     display.push({
-      title: app_list[i],
-      col_type: "text_1",
+      title: ' ““””<font color="'+getColor()+'">'+app_list[i]+'</font>',
+      col_type: "text_center_1",
       url: "hiker://empty",
     });
-    let app_detail = JSON.parse(data.app_list[i]);
+    let app_detail = eval("data."+app_list[i]);
     let names = app_detail.name;
     for (let j in names) {
       display.push({
@@ -20,6 +33,9 @@ function F() {
         pic_url: app_detail.pic_url[j],
       });
     }
+    display.push({
+      col_type:"line",
+    })
   }
 
   setResult(display);
